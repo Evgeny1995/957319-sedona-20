b5b4bedf3ddb17136d836e3f886ecaf789ce99c4
Evgeny1995 <46008208+Evgeny1995@users.noreply.github.com>
2020-09-02 08:58:42 +0300

    доделки

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+var navMenu = document.querySelector('.page-heder__icon-menu');
+var list = document.querySelector(".page-header__list-navigation");
+var menu__open = document.querySelector(".menu__open");
+var menu__close = document.querySelector(".list-navigation__btn_close");
+
+navMenu.addEventListener('click',function(){
+  if (list.classList.contains('list-navigation')){
+    list.classList.remove('list-navigation');
+    list.classList.add("menu__open");}
+});
+
+menu__close.addEventListener('click',function(){
+  console.log("i am here");
+if (list.classList.contains('menu__open')){
+    list.classList.remove ('menu__open');
+    list.classList.add ('list-navigation');}
+});
